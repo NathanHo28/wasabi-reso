0d7c5455cf8c8b1824d5e86b9a9c9681488f1864
NATHAN <nathan.ho28@gmail.com>
2015-01-01 20:05:55 -0500

    Generate Reservation model

---
+++ db/migrate/20150102005500_create_reservations.rb
@@ -0,0 +1,15 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+    	t.datetime :time
+      t.integer :party_size
+      t.integer :restaurant_id
+      t.string :name
+      t.string :email
+      t.string :request
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122185101) do
+ActiveRecord::Schema.define(version: 20150102005500) do
+
+  create_table "reservations", force: true do |t|
+    t.datetime "time"
+    t.integer  "party_size"
+    t.integer  "restaurant_id"
+    t.string   "name"
+    t.string   "email"
+    t.string   "request"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
